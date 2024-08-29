707758b00149c51597f32d0c8ad58bfcef9638df
Matthew-h1 <matthewh55@gmail.com>
2024-08-28 18:45:14 -0700

    Add files via upload

---
+++ backup.js
@@ -0,0 +1,10 @@
+// today date 
+const today = new Date();
+document.getElementById("current-time").innerHTML = today.toDateString(); 
+
+const today = new Date();
+const months =  ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+document.getElementById("current-time").innerHTML = today.toDateString(); 
